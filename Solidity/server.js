11c46fb0dbdb415f00caba74c0ec732da443fd5b
SauravRana <sauravrana1983@hotmail.com>
2019-03-07 15:55:28 +0530

    Added Two functions

--- Solidity/server.js
+++ Solidity/server.js
@@ -1,6 +1,6 @@
-import fs from 'fs';
 import Web3 from 'web3';
 import chalk from 'chalk';
+
 
 //setting up the connection
 const rpcURL = "http://127.0.0.1:8545"
@@ -10,14 +10,45 @@ const web3 = new Web3(rpcURL);
 // console.log(chalk.white(accounts));
 
 // get the address from the blockchain
-var firstAddress = '';
-web3.eth.getAccounts().then(function(data){
-  firstAddress = data[0];
-  web3.eth.getBalance(firstAddress).then(function(balance){
-    console.log(chalk.green(balance));
-  });
-  console.log(chalk.yellow(firstAddress));
-})
+// var firstAddress = '';
+// web3.eth.getAccounts().then(function(data){
+//   firstAddress = data[0];
+//   web3.eth.getBalance(firstAddress).then(function(balance){
+//     console.log(chalk.green(balance));
+//   });
+//   console.log(chalk.yellow(firstAddress));
+// })
+
+async function getCurrentAccounts(){
+  const accounts = await web3.eth.getAccounts();
+  accounts.forEach(function(value, index){
+     console.log(chalk.yellow('Account Number [' + index +  '] : ' + value ));
+  })
+  // console.log(chalk.yellow(JSON.stringify(accounts)));
+  console.log(chalk.yellow('First Account : ' + accounts[0]));
+}
+
+function createNewAccount(){
+  const newAccount = web3.eth.accounts.create();
+  console.log(chalk.green('New Address Created: ' + newAccount.address));
+}
+
+// async function unlockAccount(accountAddress, password){
+//   console.log(chalk.white("######  Unlocking the newly created account ########"));
+//   let status = await web3.eth.personal.unlockAccount(accountAddress, password, 600);
+//   return status;
+// }
+
+async function createNewAccountNode(){
+  const password = 'Password'
+  const newAccount = await web3.eth.personal.newAccount(password);
+  console.log(chalk.cyan(newAccount));
+  console.log(chalk.white("######  Unlocking the newly created account ########"));
+  let status = await web3.eth.personal.unlockAccount(newAccount, password, 600);
+  console.log(chalk.green('Account unlock status: ' + status));
+}
+
+
 
 /**
  * Account {
@@ -26,8 +57,9 @@ web3.eth.getAccounts().then(function(data){
             accounts: null
           }
  */
-const newAccount = web3.eth.accounts.create();
-console.log(chalk.green('New Address Created: ' + newAccount.address));
+
+getCurrentAccounts();
+// createNewAccountNode();
 
 const privateKey = '0xbc47da09dbdbbd702284ca9d1c40307ccb89c087a3c701f258689e4f87e4fb50';
 const address = '0xCCf0461B9fae7090B9dEc544fBAe3D5ef1fF419d';
