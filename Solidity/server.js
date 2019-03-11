4796b96dd0f5c43f13ce679846e0a9290cceb886
SauravRana <sauravrana1983@hotmail.com>
2019-03-11 11:56:45 +0530

    Deployed Contract

--- Solidity/server.js
+++ Solidity/server.js
@@ -1,23 +1,12 @@
 import Web3 from 'web3';
 import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
 
 
 //setting up the connection
 const rpcURL = "http://127.0.0.1:8545"
 const web3 = new Web3(rpcURL);
-// const accounts = new Accounts(rpcURL);
-
-// console.log(chalk.white(accounts));
-
-// get the address from the blockchain
-// var firstAddress = '';
-// web3.eth.getAccounts().then(function(data){
-//   firstAddress = data[0];
-//   web3.eth.getBalance(firstAddress).then(function(balance){
-//     console.log(chalk.green(balance));
-//   });
-//   console.log(chalk.yellow(firstAddress));
-// })
 
 async function getCurrentAccounts(){
   const accounts = await web3.eth.getAccounts();
@@ -25,7 +14,7 @@ async function getCurrentAccounts(){
      console.log(chalk.yellow('Account Number [' + index +  '] : ' + value ));
   })
   // console.log(chalk.yellow(JSON.stringify(accounts)));
-  console.log(chalk.yellow('First Account : ' + accounts[0]));
+  // console.log(chalk.yellow('First Account : ' + accounts[0]));
 }
 
 function createNewAccount(){
@@ -39,6 +28,8 @@ function createNewAccount(){
 //   return status;
 // }
 
+
+// Creating a new account node
 async function createNewAccountNode(){
   const password = 'Password'
   const newAccount = await web3.eth.personal.newAccount(password);
@@ -49,7 +40,68 @@ async function createNewAccountNode(){
 }
 
 
+// How to deploy a contract programmatically
+async function deployContract(){
 
+  let abiPath = path.join(__dirname + '/bin/DataStorage.abi');
+  let binPath = path.join(__dirname + '/bin/DataStorage.bin');
+
+  console.log(chalk.green(abiPath));
+  console.log(chalk.green(binPath));
+
+  let abi = fs.readFileSync(abiPath);
+  let bin = '0x' + fs.readFileSync(binPath);
+
+
+  let contract = new web3.eth.Contract(JSON.parse(abi));
+
+  console.log()
+  // Returns an object of abstract contract.
+  let status = await contract.deploy({
+    data: bin,
+    arguments: [100]
+  }).send({
+    from: '0xfD5bd9e6f31301bA31D9ddC2426E649b3edb7d94',
+    gasPrice: 1000,
+    gas: 300000
+  })
+
+  console.log(chalk.red('Address of Contract Deployed : ' + status.options.address));
+}
+
+
+async function readMethodOfContract(){
+  let deployedContract = getContractObject();
+
+  console.log(chalk.bgWhite(chalk.black('######## Details of Deployed Contract ###########')));
+  console.log(chalk.green('Contract Address : ' + deployedContract.options.address));
+  console.log(chalk.green('Address used to deploy : ' + deployedContract.options.from));
+
+  console.log(chalk.white("######  Method Invocation ########"));
+  console.log(chalk.yellow('Invoking function getData() on contract : ' + deployedContract.methods.getData()));
+  console.log(chalk.green('====== This is just spitting out the code that should be invoked. ======'));
+
+  let output = await deployedContract.methods.getData().call();
+  console.log(chalk.yellow('Invoking function getData() on contract : ' + output));
+}
+
+/**
+ * Creating a utility function to get the contract object.
+ */
+function getContractObject(){
+  let abiPath = path.join(__dirname + '/bin/DataStorage.abi');
+  let abi = fs.readFileSync(abiPath);
+
+  // Replace the contract address with the contract you want to invoke.
+  let contractAddress = '0xadb1e7fea9a24daee48492c3523721ea6b42f271';  // address of the deployed contract
+  let options = {
+    from: '0xfD5bd9e6f31301bA31D9ddC2426E649b3edb7d94',
+    gasPrice: 1000,
+    gas: 300000
+  };
+  let deployedContract = new web3.eth.Contract(JSON.parse(abi),contractAddress,options);
+  return deployedContract;
+}
 /**
  * Account {
               address: '0xCCf0461B9fae7090B9dEc544fBAe3D5ef1fF419d',
@@ -58,8 +110,12 @@ async function createNewAccountNode(){
           }
  */
 
-getCurrentAccounts();
+// getCurrentAccounts();
+
+
 // createNewAccountNode();
+// deployContract();
+readMethodOfContract();
 
 const privateKey = '0xbc47da09dbdbbd702284ca9d1c40307ccb89c087a3c701f258689e4f87e4fb50';
 const address = '0xCCf0461B9fae7090B9dEc544fBAe3D5ef1fF419d';
