c22e4327a0594488609f40ee501aab74b46b0ae9
SauravRana <sauravrana1983@hotmail.com>
2019-03-12 12:47:07 +0530

    Added Imports and added logic folder

---
+++ Solidity/Events/logic/eventSubscriber.js
@@ -0,0 +1,7 @@
+import Web3 from 'web3';
+import chalk from 'chalk';
+import fs from 'fs';
+import path from 'path';
+import {setDataMethodofContract} from './coreObject';
+
+console.log(chalk.green('==> Subscribing to events on a contract'));
